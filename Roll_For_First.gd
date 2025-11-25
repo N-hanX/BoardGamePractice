b531778afd8c3886da859074231cc886f7ddd4c0
N-hanX <199831591+N-hanX@users.noreply.github.com>
2025-11-25 13:19:31 -0800

    first roll feature added, dice click needs fix

--- Roll_For_First.gd
+++ Roll_For_First.gd
@@ -5,6 +5,7 @@ onready var label = $PanelContainer/VBoxContainer/Label
 onready var texture_rect = $PanelContainer/VBoxContainer/TextureRect
 onready var button = $PanelContainer/VBoxContainer/Button
 onready var go_away_timer = $GoAwayTimer
+var pink_piece_turn : bool
 
 func _ready() -> void:
 	randomize()
@@ -22,12 +23,16 @@ func _on_timer_timeout():
 		1: 
 			label.text = "Pink piece!"
 			texture_rect.texture = load("res://Art/pink piece.png")
+			pink_piece_turn = true
 		2:
 			label.text = "Blue piece!"
 			texture_rect.texture = load("res://Art/blue piece.png")
-	
+			pink_piece_turn = false
+			
+	Events.emit_signal("send_piece", pink_piece_turn)
 	go_away_timer.start()
 
 
 func _on_goAwayTimer_timeout():
 	hide()
+	Events.emit_signal("can_click")
